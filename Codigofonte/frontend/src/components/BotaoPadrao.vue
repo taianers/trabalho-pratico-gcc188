4813e4caee338d48576cf8edde34a8e6eaa49815
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2021-11-23 01:30:45 -0300

    Adiciona botao

---
+++ Codigofonte/frontend/src/components/BotaoPadrao.vue
@@ -0,0 +1,76 @@
+<template>
+  <button
+    @click="click($event)"
+    :class="[
+      'botao',
+      aparencia == 'primario' ? 'botao-primario' : 'botao-secundario',
+      arredondado ? 'botao-arredondado' : 'botao-container',
+    ]"
+  >
+    {{ titulo }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "BotaoPadrao",
+  props: {
+    titulo: String,
+    aparencia: {
+      type: String,
+      default: "primario",
+    }, // primario ou secundario
+    arredondado: Boolean,
+  },
+  methods: {
+    click(e) {
+      this.$emit("click", e);
+    },
+  },
+  data() {
+    return {
+      styleBotao: {},
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.botao {
+  border: none;
+  outline: none;
+  line-height: 20px;
+  text-align: center;
+}
+.botao:hover {
+  filter: brightness(0.8);
+}
+.botao-primario {
+  background-color: #780116;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 30px;
+  text-align: center;
+  color: #ffffff;
+}
+.botao-secundario {
+  background: #ffffff;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  color: #000000;
+}
+.botao-container {
+  width: 100%;
+  max-width: 700px;
+  padding: 16px;
+  border-radius: 10px;
+}
+.botao-arredondado {
+  border-radius: 30px;
+  padding: 16px 45px;
+}
+</style>
